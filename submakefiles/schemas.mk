15924e17e20cce8053756067eea8d209f1febddc
Jon Chesterfield <jonathanchesterfield@gmail.com>
2023-11-26 11:53:48 +0000

    comments in schemas makefile

--- submakefiles/schemas.mk
+++ submakefiles/schemas.mk
@@ -1,10 +1,24 @@
-# The idea here is to allow relaxng schemas in compact or xml formats
-# That's reasonably messy in make so is factored out. This file defines
-# various targets and variables prefixed SCHEMA and one function that takes
-# a %-prefixed name of a schema from somewhere in the MAKEFILE_DIR rooted tree:
+# The idea here is to allow relax ng schemas in compact or xml formats.
+# If both exist, editing either will update the other.
+# If only one format exists the rest of the system can use either format.
+# On ambiguity on what to do, the xml format schema wins.
+# Driven by suffix, *.rng is xml syntax and *.rnc is compact syntax.
+# 
+# This file defines one function that takes a %-prefixed name of a schema from
+# somewhere in the MAKEFILE_DIR rooted tree:
 # $(call get_schema_name, %derived.rnc)
-# that returns the name of an up to date copy of that target, even if the only
-# source for that schema in tree is a .rng format
+# The call returns the name of an up to date copy of that target, even if the only
+# source for that schema in tree is in the the other format, which can be used
+# for xmllint etc
+#
+# As a special case, if both exist and one is empty, it'll be updated from the other.
+# This means one can "fix" a failed partial edit by clearing the current file and
+# calling make to clobber it with the previous version. It also means one can create
+# a file in the other format, next to the current one, populate that with make, edit
+# it with changes reflected in the original, then delete it when done.
+#
+# If both formats are present, running this script is prone to putting them both in
+# the output format of trang, which is roughly a pretty-printed / canonical form.
 
 # Have the caller specify the root of the tree so this file can be hidden
 # under a subdirectory. Will only find schema under this root.
@@ -29,11 +43,11 @@ SCHEMA_RAW_RNG_SOURCE := $(filter-out $(MAKEFILE_DIR)/$(SCHEMA_HIDDEN_DIR)%, $(c
 
 # Derive names under XML_DIR
 SCHEMA_CHDIR =	$(subst $(MAKEFILE_DIR)/, $(SCHEMA_XML_DIR), $1)
+SCHEMA_UPDATED_RNG := $(call SCHEMA_CHDIR, $(SCHEMA_RAW_RNG_SOURCE))
 SCHEMA_UPDATED_RNC := $(call SCHEMA_CHDIR, $(SCHEMA_RAW_RNC_SOURCE:.rnc=.rng))
-SCHEMA_UPDATED_RNG := $(call SCHEMA_CHDIR, $(SCHEMA_RAW_RNG_SOURCE:.rng=.rng))
 
 # Going to make one file for each schema under XML_DIR
-SCHEMA_UPDATED := $(sort $(SCHEMA_UPDATED_RNC) $(SCHEMA_UPDATED_RNG))
+SCHEMA_UPDATED := $(sort $(SCHEMA_UPDATED_RNG) $(SCHEMA_UPDATED_RNC))
 
 # And then going to extract a RNG and a RNC format from it
 SCHEMA_DERIVED_RNG := $(subst $(SCHEMA_XML_DIR), $(SCHEMA_RNG_DIR), $(SCHEMA_UPDATED))
@@ -47,23 +61,23 @@ SCHEMA_DERIVED_RNC := $(patsubst %.rng, %.rnc, $(subst $(SCHEMA_XML_DIR), $(SCHE
 # running the receipe clobbers the other candidate input file
 
 $(SCHEMA_UPDATED_RNG):: $(SCHEMA_XML_DIR)%.rng:	%.rng
+	@mkdir -p $(dir $@)
 #	@echo "Updating $@ from $<"
 	@if ! test -s $< ; then trang -Irnc -Orng $(<:.rng=.rnc) $< ; fi
-	@mkdir -p $(dir $@)
 	@cp $< $@
 	@$(if $(filter $@, $(SCHEMA_UPDATED_RNG)), @trang -Irng -Ornc $@ $(<:.rng=.rnc))
 	@touch $@
 
 $(SCHEMA_UPDATED_RNC):: $(SCHEMA_XML_DIR)%.rng:	%.rnc
+	@mkdir -p $(dir $@)
 #	@echo "Updating $@ from $<"
 	@if ! test -s $< ; then trang -Irng -Ornc $(<:.rnc=.rng) $< ; fi
-	@mkdir -p $(dir $@)
 	@trang -Irnc -Orng $< $@
 	@$(if $(filter $@, $(SCHEMA_UPDATED_RNG)), cp $@ $(<:.rnc=.rng))
 	@touch $@
 
-# Made the XML dir one, extract the two formats from it
-# Could simplify slightly by deleting the XML subdirectory
+# Made the single up to date file from one or both of the inputs. Derive the
+# output in each format from it.
 
 $(SCHEMA_DERIVED_RNG): $(SCHEMA_RNG_DIR)%.rng:	$(SCHEMA_XML_DIR)%.rng
 	@mkdir -p $(dir $@)
