6b8439f8278faf9d3c53d0bb67efa2dfa2427a60
Jon Chesterfield <jonathanchesterfield@gmail.com>
2024-01-16 15:12:50 +0000

    Generate multiple independent websites from a list of projects

--- Planning/Planning.mk
+++ Planning/Planning.mk
@@ -1,10 +1,11 @@
 obsidian_dir := ${HOME}/Documents/Obsidian
-planning_src := ${obsidian_dir}/PlanningWIP
+
 output_dir := ${obsidian_dir}/Projects
+
 WORKDIR := /tmp/.ObsidianXML
 
+# TODO: Read these from somewhere under obsidian_dir
 project_names := PlanningWIP AMD
-
 
 planning_src := $(addprefix ${obsidian_dir}/,${project_names})
 
@@ -18,9 +19,6 @@ planning_src := $(addprefix ${obsidian_dir}/,${project_names})
 # might be able to work in terms of src with a common prefix instead of filtering out
 # the root
 
-# Currently this changes the prefix from some source tree to a working tree, might be
-# simpler to prefix with a fixed workdir which is subtracted at the corresponding places
-
 SPACE_ESC := +
 SPACE_ESCAPED_SRC := $(shell find $(planning_src) -type f -name "*md" | sed 's/ /$(SPACE_ESC)/g')
 
@@ -28,6 +26,7 @@ SPACE_ESCAPED_SRC := $(shell find $(planning_src) -type f -name "*md" | sed 's/ 
 SPACE_ESCAPED_DIRS := $(shell find $(planning_src) -mindepth 1 -type d | sed 's/ /$(SPACE_ESC)/g')
 
 # Get names in some corresponding work directory
+# Simpler to prefix than to substitute
 WORKDIR_ESCAPED_SRC := $(addprefix $(WORKDIR),$(SPACE_ESCAPED_SRC))
 WORKDIR_ESCAPED_DIRS := $(addprefix $(WORKDIR),$(SPACE_ESCAPED_DIRS))
 
@@ -36,10 +35,6 @@ $(WORKDIR):
 
 clean::
 	@rm -rf $(WORKDIR)
-
-# $(info "for e. in" $(WORKDIR_ESCAPED_SRC))
-# $(info "get $(WORKDIR)/%%.md")
-# $(info "if a. change in" $(subst $(SPACE_ESC),\ ,$(SPACE_ESCAPED_SRC)))
 
 # Seems to be difficult to rely on a single file containing spaces and easy to depend on all of them
 ALL_SRC_WITH_SPACES := $(subst $(SPACE_ESC),\ ,$(SPACE_ESCAPED_SRC))
@@ -64,9 +59,8 @@ COLLECTED_TASKS := $(WORKDIR_ESCAPED_DIRS:=.card.xml)
 # applicable to the directory identified by the stem
 $(COLLECTED_TASKS):	$(WORKDIR)%.card.xml:	$(DERIVED_CARDS)
 	@mkdir -p $(@D)
-#	echo '<?xml version="1.0" encoding="UTF-8"?>' > $@
 	@echo '' > $@
-	@echo '<card name="$(notdir $*)">' >> $@
+	@echo '<card name="$(notdir $(subst $(SPACE_ESC), ,$*))">' >> $@
 # cat hangs if the list of files is empty and dev null contributes nothing
 	@cat /dev/null $(sort $(filter $(WORKDIR)$*/%,$(DERIVED_CARDS))) >> $@
 	@echo '</card>' >> $@
@@ -78,32 +72,27 @@ $(PROJECT_XML):	$(WORKDIR)/%.cards.xml:	$(COLLECTED_TASKS)
 	@cat /dev/null $(sort $(filter $(WORKDIR)${obsidian_dir}/$*/%,$(COLLECTED_TASKS))) >> $@
 	@echo '</Project>' >> $@
 
-$(WORKDIR)/Planning.cards.xml:	$(COLLECTED_TASKS)
-	@echo '<?xml version="1.0" encoding="UTF-8"?>' > $@
-	@echo '<Project name="Planning">' >> $@
-#	Sort isnt right here but it's predictable for now
-	@cat $(sort $(COLLECTED_TASKS)) >> $@
-	@echo '</Project>' >> $@
 
+PROJECT_HTML := $(addprefix $(WORKDIR)/,$(addsuffix .html,$(project_names)))
+PUBLISH_HTML := $(addprefix ${output_dir}/,$(addsuffix .html,$(project_names)))
 
-PROJECT_HTML := $(addprefix Planning/,$(addsuffix .html,$(project_names)))
-
-$(PROJECT_HTML):	Planning/%.html:	$(WORKDIR)/%.cards.xml card_to_html.xsl
+$(PROJECT_HTML):	%.html:	%.cards.xml card_to_html.xsl
 	@xsltproc $(XSLTPROCOPTS) --output "$@" card_to_html.xsl "$<"
 
-# Only copies changed files to not retrigger sync
+# Only copies changed files to not retrigger obsidian sync
+$(PUBLISH_HTML):	${output_dir}/%.html:	$(WORKDIR)/%.html
+	@cmp "$@" "$<" >/dev/null 2>&1 || cp "$<" "$@"
 
-.PHONY:	publish
-publish:	$(PROJECT_HTML)
-#	TODO: derive the copy invocations from the project names
-	@cmp Planning/PlanningWIP.html ${output_dir}/PlanningWIP.html >/dev/null 2>&1 || cp Planning/PlanningWIP.html ${output_dir}/PlanningWIP.html
-	@cmp Planning/AMD.html ${output_dir}/AMD.html >/dev/null 2>&1 || cp Planning/AMD.html ${output_dir}/AMD.html
+# TODO: Work out if the dependency graph is sound for this to isolate timestamps
+# from errors in this makefile
+projects: $(PUBLISH_HTML)
 
+publish:	projects
 #	This is a hack. Renaming files in the obsidian dir don't reliably trigger
 # 	a rebuild of planning - something is wrong in the dependency graph
-#	or using time based rebuild across multiple systems is a bad thing
-#	Deleting the work dir after the build makes the next scheduled run correct
-	@rm -rf $(WORKDIR) Planning.html
+#	(or using time based rebuild across multiple systems is a bad thing)
+#	Deleting the work dir after the build makes the next scheduled make correct
+	@rm -rf $(WORKDIR)
 
 clean::
 	@rm -f $(PROJECT_HTML)
