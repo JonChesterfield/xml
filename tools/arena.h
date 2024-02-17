139ed4c834a6cfc769db70f6b5a8ef12e72458d8
Jon Chesterfield <jonathanchesterfield@gmail.com>
2024-02-17 21:07:49 +0000

    iterating on arena interface

--- tools/arena.h
+++ tools/arena.h
@@ -2,6 +2,26 @@
 #define TOOLS_ARENA_H
 
 // Interface sketch only so far.
+// Thinking a pointer to contiguous memory, where allocate is bump pointer
+// Exceeding the initial allocated size could be an error or allocate more
+// If allocating more, some implementations will want to change the base address
+// and some others won't need to.
+// Idea is reallocate as allocate + memcpy in some cases.
+//
+// Considering various backing memory types:
+// 1. mmap
+// 2. brk
+// 3. .data objects
+// 4. malloc/realloc
+// All of these can do a (limited) sort of deallocation
+// Most can be resized, some can't
+// The base pointer might be invalidated on resize but might not be
+//
+// Most interfaces are more convenient in terms of a pointer but the
+// resize-invalidates-pointer property argues for offsets.
+//
+// Alignment could be treated as a separate allocation of a few bytes,
+// put it in the public API but not in the implementation API
 
 #define ARENA_CONTRACT() 0
 
@@ -12,20 +32,32 @@ typedef const struct arena_module_ty *arena_module;
 static inline void *arena_create(arena_module mod, size_t N);
 static inline void arena_destroy(arena_module mod, void *s);
 
-// Primary function, may increase size of backing storage
-static inline void *arena_allocate(arena_module mod, void *s, uint64_t bytes);
-
 // Bytes already allocated
 static inline size_t arena_size(arena_module mod, void *s);
 
 // Bytes available in total
 static inline size_t arena_capacity(arena_module mod, void *s);
 
+// True if this arena can be resized after allocation
+static inline bool arena_resizable(arena_module mod);
+
+// Get pointer to the current base of the memory of the arena
+static inline void * arena_base_address(arena_module mod, void*);
+
+// Get pointer to the next object that will be allocated
+static inline void * arena_next_address(arena_module mod, void*);
+
+// Allocate without reallocating, asserts on insufficient capacity, else UB. Doesn't change base_address.
+static inline uint64_t arena_allocate_assuming_capacity(arena_module mod, void *s, uint64_t bytes);
+
+
+// Primary function, may increase size of backing storage in which case base_address is invalidated
+static inline uint64_t arena_allocate(arena_module mod, void *s, uint64_t bytes);
+
+
 // Increase capacity
 static inline void *arena_reserve(arena_module mod, void *s, size_t bytes);
 
-// Allocate without the branch to reserve
-static inline void *arena_allocate_assuming_capacity(arena_module mod, void *s, uint64_t bytes);
 
 struct arena_module_ty {
   void *(*const create)(void);
