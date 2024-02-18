65d5c2190b091e793588e28dd2da1c0c3e9d8ca7
Jon Chesterfield <jonathanchesterfield@gmail.com>
2024-02-18 23:51:45 +0000

    better contract handling, in particular strategically zeroing jmp_buf makes unexpected contract failures more obvious

--- tools/arena.c
+++ tools/arena.c
@@ -9,12 +9,20 @@
 
 static jmp_buf death_buffer;
 
-static const struct arena_module_ty arena_libc_jmp = ARENA_MODULE_INIT(arena_libc, contract_active, &death_buffer);
+static const struct arena_module_ty arena_libc_jmp = ARENA_MODULE_INIT(arena_libc, contract_unit_test, &death_buffer);
 
 static const arena_module mod = &arena_libc_jmp;
 
 
 static MODULE(create_destroy) {
+
+  TEST("sanity check jmp buf")
+    {
+      CHECK(contract_jmpbuf_is_zero(&death_buffer));
+      contract_jmpbuf_set_zero(&death_buffer);
+      CHECK(contract_jmpbuf_is_zero(&death_buffer));
+    }
+  
   TEST("size 0") {
     arena_t a = arena_create(mod, 0);
     CHECK(arena_valid(mod, a));
@@ -40,19 +48,23 @@ static MODULE(create_destroy) {
     arena_destroy(mod, a);
   }
 
-  #if 0
   TEST("fail a precondition")
     {
       arena_t a = {0};
       DEATH(death_buffer, arena_destroy(mod, a));
     }
-  #endif
 
+#if 0
   TEST("fail without the death macro")
     {
+      // This was checking the behaviour of failing a contract after forgetting
+      // to wrap it in DEATH() - previously segv, now a stderr message that
+      // a jmp_buf is zero and an early exit 1
       arena_t a = {0};
       arena_destroy(mod, a);
     }
+#endif
+
 
 }
 
--- tools/contract.h
+++ tools/contract.h
@@ -15,11 +15,60 @@
 
 #if __STDC_HOSTED__
 #include <setjmp.h>
+
+static inline bool contract_jmpbuf_is_zero(jmp_buf * buf)
+{
+  _Static_assert(sizeof(jmp_buf) % 8 == 0, "");
+  bool ret = true;
+
+#if 1
+  // Requires may_alias attribute, gives better codegen
+  typedef uint64_t __attribute__((__may_alias__)) aliasing_uint64;
+  
+  aliasing_uint64 * cursor = (aliasing_uint64*)buf;
+
+  for (size_t i = 0; i < sizeof(jmp_buf)/8; i++)
+    {
+      ret &= (cursor[i] == 0);
+    }
+#else
+
+
+  unsigned char * cursor = (unsigned char *)buf;
+
+#ifdef __has_builtin
+#if __has_builtin( __builtin_assume_aligned)
+  // damage mitigation
+  cursor = __builtin_assume_aligned(cursor, _Alignof(jmp_buf));
+#endif
+#endif
+  for (size_t i = 0; i < sizeof(jmp_buf); i++)
+    {
+      ret &= (cursor[i] == 0);
+    }
+#endif
+  
+  return ret;
+}
+
+static inline void contract_jmpbuf_set_zero(jmp_buf * buf)
+{
+  jmp_buf tmp = {0};
+  
+  unsigned char * src = (unsigned char *)&tmp;
+  unsigned char * dst = (unsigned char *)buf;
+  for (size_t i = 0; i < sizeof(jmp_buf); i++)
+    {
+      dst[i] = src[i];
+    }
+}
+
 #endif
 
 
 static inline void contract_active(bool, void*, const char*, size_t);
 static inline void contract_discard(bool, void*, const char*, size_t);
+static inline void contract_unit_test(bool, void*, const char*, size_t);
 
 static inline void contract_write(bool, void*, const char*, size_t);
 static inline void contract_exit(bool, void*, const char*, size_t);
@@ -27,6 +76,7 @@ static inline void contract_longjmp(bool, void*, const char*, size_t);
 
 static inline void contract_active(bool expr, void* maybe_jmp_buf, const char *message, size_t message_length)
 {
+  // Write warnings, longjmp if a buffer is available, exit regardless
   if (!expr)
     {
       contract_write(expr, maybe_jmp_buf, message, message_length);
@@ -34,7 +84,18 @@ static inline void contract_active(bool expr, void* maybe_jmp_buf, const char *m
       contract_exit(expr, maybe_jmp_buf, message, message_length);
     }
 }
-  
+
+static inline void contract_unit_test(bool expr, void* maybe_jmp_buf, const char *message, size_t message_length)
+{
+  // Variant more appropriate for unit tests - no printing unless longjmp is called out of context
+  if (!expr)
+    {
+      contract_longjmp(expr, maybe_jmp_buf, message, message_length);
+      contract_exit(expr, maybe_jmp_buf, message, message_length);
+    }
+}
+
+
 static inline void contract_discard(bool expr, void* maybe_jmp_buf, const char *message, size_t message_length)
 {
   (void)expr;
@@ -73,14 +134,27 @@ static inline void contract_longjmp(bool expr, void* maybe_jmp_buf, const char *
 #if __STDC_HOSTED__
   (void)message;
   (void)message_length;
-  if (!expr)
+  if (maybe_jmp_buf)
     {
-      if (maybe_jmp_buf)
-      {
-        jmp_buf * buf = maybe_jmp_buf;      
-        longjmp(*buf, 1);
-      }
+      if (contract_jmpbuf_is_zero(maybe_jmp_buf))
+        {
+          contract_write(false, 0, "Warning: Zero jmp_buf\n", sizeof("Warning: Zero jmp_buf\n")-1);
+          contract_write(expr, 0, message, message_length);
+          return;
+        }
+      
+      if (!expr)
+        {
+          jmp_buf * buf = maybe_jmp_buf;
+          longjmp(*buf, 1);
+        }
+      else
+        {
+          // reasonable point to reset it to zero
+          contract_jmpbuf_set_zero(maybe_jmp_buf);
+        }
     }
+  
 #else
   contract_discard(expr, maybe_jmp_buf, message, message_length);  
 #endif
