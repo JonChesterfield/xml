fec991990eb74f546a34066646c7720ebcf66883
Jon Chesterfield <jonathanchesterfield@gmail.com>
2024-03-15 09:45:12 +0000

    change to greedy, noticed some missing detection of invalid regex

--- regex/regex_interpreter.c
+++ regex/regex_interpreter.c
@@ -51,66 +51,81 @@ uint64_t regex_interpreter_with_context_string_matches(regex_cache_t *cache,
   stringtable_index_t current =
       regex_cache_insert_regex_bytes(cache, (const char *)regex, regex_len);
   if (!stringtable_index_valid(current)) {
-    #if 0
-    // Appears canonicalise fails to compute a fixpoint
-    printf("not recognised: %lu\n", current.value);
-
-    ptree_context ctx = regex_ptree_create_context();
-    ptree p = regex_from_char_sequence(ctx, regex, regex_len);
-    if (ptree_is_failure(p)) {
-      printf("didn't look like a regex\n");
-    } else {
-      regex_ptree_as_xml(&stack_libc, stdout, p);
-    }
-
-    ptree c = regex_canonicalise(ctx, p);
-    printf("canon:\n");
-    regex_ptree_as_xml(&stack_libc, stdout, c);
-
-    if (!ptree_is_failure(c)) {
-      if (!regex_is_canonical(c)) {
-        printf("non-canonical canonical\n");
-      }
-      current = regex_cache_insert_regex_canonical_ptree(cache, c);
-      printf("not recognised2 ?: %lu\n", current.value);
-    }
-    
-    #endif
     return regex_unrecognised;
   }
 
-
+  const char * r = stringtable_lookup(&cache->strtab, current);
+  printf("Matching regex %s/%s len %lu\n", regex,r,regex_len);
   
   // context handling here would be much improved by a clear-and-reuse model
   for (unsigned iter = 0; iter < target_len; iter++) {
     ptree_context ctx = regex_ptree_create_context();
     assert(regex_ptree_valid_context(ctx));
+
+    bool current_nullable = false;
+    
     {      
       ptree p = regex_from_stringtable(&cache->strtab, current, ctx);
 
       const char * r = stringtable_lookup(&cache->strtab, current);      
-      
+
       if (regex_nullable_p(ctx, p)) {
-        regex_ptree_destroy_context(ctx);
-        printf("Iter %u, %s. Nullable\n", iter, r);
-        return iter;
+        // return iter here is eager
+        current_nullable = true;
       }
 
-      if (regex_is_empty_set(p)) {
+      bool empty_set = regex_is_empty_set(p);
+      bool empty_string = regex_is_empty_string(p);
+      if (empty_set) {assert(!empty_string);}
+      if (empty_string) {assert(!empty_set);}
+      
+      if (empty_set) {
         regex_ptree_destroy_context(ctx);
         printf("Iter %u, %s. Failure\n", iter, r);
         return match_failure;
       }
+
+      
+      if (empty_string) {
+        printf("Iter %u, %s. Empty string\n", iter, r);
+        return iter;
+      }
+
+      printf("Iter %u, %s. Continue\n", iter, r);
+        
+      
     }
 
     unsigned char byte = target[iter];
-    current = regex_cache_calculate_derivative(cache, current, byte);
+    stringtable_index_t next = regex_cache_calculate_derivative(cache, current, byte);
 
+    if (current_nullable)
+      {
+        ptree n = regex_from_stringtable(&cache->strtab, next, ctx);
+        if (regex_is_empty_set(n)) {
+          const char * r = stringtable_lookup(&cache->strtab, current);
+          printf("Iter %u, %s. Next last nullable\n", iter, r);
+          return iter;
+        }
+      }
+    
+    current = next;
     regex_ptree_destroy_context(ctx);
   }
 
-
-  return match_failure;
+  {
+  ptree_context ctx = regex_ptree_create_context();
+  bool nullable = regex_nullable_p(ctx, regex_from_stringtable(&cache->strtab, current, ctx));
+  regex_ptree_destroy_context(ctx);
+  
+  if (nullable) {
+    printf("reached end of input, current is nullable\n");
+    return target_len;
+  } else {
+    printf("reached end of input, current is not nullable\n");
+    return match_failure;
+  }
+  }
 }
 
 uint64_t regex_interpreter_string_matches(const unsigned char *regex,
--- regex/regex_match_tests.c
+++ regex/regex_match_tests.c
@@ -14,30 +14,48 @@ struct regex_case {
   uint64_t expect;
 };
 
-static void dump_case(struct regex_case s, uint64_t res) {
-#if 1
-  printf("\n%s ", s.reg);
-  for (size_t i = 0; i < s.sz_str; i++) {
-    printf("%02x", s.str[i]);
+static void dump_val(uint64_t val) {
+  const char * str = 0;
+  switch(val)
+    {
+    case match_failure:
+      str = "failure"; break;
+    case machine_failure:
+      str = "machine"; break;
+    case regex_unrecognised:
+      str = "invalid"; break;
+    default:
+      break;    
+    }
+  if (str) {
+    printf("%s", str);
+  } else {
+    printf("%lu", val);
   }
-  printf(" -> %lu != %lu\n", s.expect, res);
-#else
-  printf("\n%s/%zu ", s.reg, s.sz_reg);
-  for (size_t i = 0; i < s.sz_str; i++) {
-    printf("%02x", s.str[i]);
-  }
-  printf("/%zu -> %lu != %lu\n", s.sz_str, s.expect, res);
-#endif
 }
 
-// Mainly to allow embedded zeros
-#define R(X) X, (sizeof(X) - 1)
+static void dump_case(struct regex_case s, uint64_t res) {
+  printf("reg %s sbj ", s.reg);
+  for (size_t i = 0; i < s.sz_str; i++) {
+    printf("%02x", s.str[i]);
+  }
+  printf(" -> [ex ");
+  dump_val(s.expect);
+  printf("] != [res ");
+  dump_val(res);
+  printf("]\n");
+
+}
+
+// Allows embedded 0s vs using strlen
+#define R(X) X, (sizeof(X)-1)
 
 MODULE(regex_match) {
 
   TEST("wip") {
     regex_cache_t cache = regex_cache_create();
     CHECK(regex_cache_valid(cache));
+    printf("length of \x01 is %lu\n", strlen("\x01"));
     struct regex_case cases[] = {
         {
             R("01"),
@@ -45,31 +63,58 @@ MODULE(regex_match) {
             1,
         },
         {
-            R("(:0102)"),
+            R("01"),
             R("\x01\x02"),
+            1,
+        },
+        {
+            R("01"),
+            R("\x01\x01"),
+            1,
+        },
+        {
+            R("0101"),
+            R("\x01"),
+            match_failure,
+        },
+
+        // This case was written in error (missing concat :)
+        // but parsed as a valid regex 01, instead of rejecting the 0101 form
+        {
+            R("0101"),
+            R("\x01"),
+            regex_unrecognised,
+        },
+        {
+            R("(:0101)"),
+            R("\x01\x02"),
+            match_failure,
+        },
+        
+        {
+            R("(:0102)"),
+            R("\x01\x02\x03"),
             2,
         },
         {
             R("(*02)"),
             R("\x02\x02\x33"),
-            1,
+            2,
         },
     };
     enum { size = sizeof(cases) / sizeof(cases[0]) };
 
     for (size_t i = 0; i < size; i++) {
-
+      printf("\n");
       CHECK(regex_in_byte_representation(cases[i].reg, cases[i].sz_reg));
       uint64_t res = regex_interpreter_with_context_string_matches(
           &cache, (const unsigned char *)cases[i].reg, cases[i].sz_reg,
           (const unsigned char *)cases[i].str, cases[i].sz_str);
 
-      dump_case(cases[i], res);
-
-      CHECK(res != match_failure);
-      CHECK(res != machine_failure);
-      CHECK(res != regex_unrecognised);
-
+      if (res != cases[i].expect) {
+        dump_case(cases[i], res);
+      }
+      
       CHECK(res == cases[i].expect);
     }
 
